f7e4d8bf35fcaaf07897e8b481fa7754419b1b67
Julianmaciasa <jmacias.argal@gmail.com>
2025-03-31 15:50:50 -0500

    Ajustes 31 de marzo

--- src/components/ClienteComponent.jsx
+++ src/components/ClienteComponent.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import  '../stylesheets/ClienteComponent.css';
 
-function ClienteComponent(props){
+
+
+function ClienteComponent(props) {
     return(
             <div className = 'contenedor-cliente'>
                 <img
-                  /*  className = 'imagen-cliente'
-                    src = {require(`../images/logo-${props.imagen}.png`)} 
-                    alt= 'logo-CODEPEC'*/
-                />
+                  className='imagen-cliente'
+                  src={`src/assets/logo${props.imagen}.jfif`}
+                  alt="Logo Reeds"/>
             <div className='contenedor-texto-cliente'>
                 <p className='nombre-cliente'>
                  <strong>{props.nombre}</strong> 
