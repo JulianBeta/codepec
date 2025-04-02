325408e6f1af75e7ddff7723c3055b0c41dfc3cb
julianmaciasa <137076111+julianmaciasa@users.noreply.github.com>
2025-04-01 20:35:16 -0500

    inicio ajustado

---
+++ src/components/Carrusel2.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import '../stylesheets/Carrusel2.css'
+import { EffectCoverflow, Pagination, Navigation, Autoplay } from 'swiper/modules';
+import slide_image_1 from './../assets/logoacees.jfif';
+import slide_image_2 from './../assets/logogrub.jfif';
+import slide_image_3 from './../assets/logomass.jfif';
+import slide_image_4 from './../assets/logomelomarket.jfif';
+import slide_image_5 from './../assets/logonaturasas.jfif';
+import slide_image_6 from './../assets/logonextdoor.jfif';
+import slide_image_7 from './../assets/logoreeds.jfif';
+// import slide_image_8 from '../../assets/logosconcursos/logoterritorial2021.jfif';
+import { Link } from 'react-router-dom';
+
+const Slider2 = () => {
+  return (
+    <Link to="/concursos"><div className="container">
+      <h1 className="heading"></h1>
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={'2'}
+        touchAngle={0}
+        touchEventsTarget='container'
+        autoplay={{
+          delay: 1000,
+          disableOnInteraction: false,
+        }}
+        coverflowEffect={{
+          rotate: 2,
+          stretch: 5,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: '.swiper-pagination', clickable: true }}
+        // navigation={{
+        //   nextEl: '.swiper-button-next',
+        //   prevEl: '.swiper-button-prev',
+        //   clickable: true,
+        // }}
+        modules={[EffectCoverflow, Pagination, Navigation, Autoplay]}
+        className="swiper_container"
+      >
+               
+        <SwiperSlide>
+        <a href='http://localhost:5173/concursos'/>
+          <img src={slide_image_1} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+        <a href='http://localhost:5173/concursos'/>
+          <img  src={slide_image_2} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_3} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_4} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_5} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_6} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_7} alt="slide_image" />
+        </SwiperSlide>
+        {/* <SwiperSlide>
+          <img src={slide_image_8} alt="slide_image" />
+        </SwiperSlide> */}
+
+        <div className="slider-controler">
+          {/* <div className="swiper-button-prev slider-arrow">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+          </div>
+          <div className="swiper-button-next slider-arrow">
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </div> */}
+          <div className="swiper-pagination"></div>
+        </div>
+      </Swiper>
+      
+        
+    </div> </Link>
+  );
+}
+
+export default Slider2;
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -4,6 +4,7 @@ import '../stylesheets/Inicio.css'
 import imagenciudad from '../assets/ciudad.jpg'
 import imagenestablecimiento from '../assets/establecimiento.jpg'
 import Slider from '../components/Carrusel'
+import Slider2 from '../components/Carrusel2'
 
 
 
@@ -20,17 +21,17 @@ export const Inicio = () => {
 <div className='cuadroserviciosinicio'>
         <Link to="/serviciosestablecimientos"><img className="imagenesinicio" src={imagenestablecimiento} alt="establecimientos" /><h3 className='textoserviciosinicio'>SERVICIOS ESTABLECIMIENTOS DE COMERCIO</h3></Link>
         </div></div>
-
+<h1 className='titulossecundariosinicio'>¿QUIÉNES SOMOS?</h1>
         <div className='seccionquienesyporque'>
-           <h1 className='titulossecundariosinicio'>¿QUIÉNES SOMOS?</h1>
+           
            <p className='textoquienesyporque'>CODEPEC S.A.S. es una firma legal especializada en la asesoría, acompañamiento, defensa y promoción de los derechos de los ciudadanos en materia comercial y urbanística. Nuestra misión es ofrecer soluciones jurídicas integrales que permitan a nuestros clientes operar dentro del marco normativo, minimizando riesgos legales y optimizando su gestión empresarial.
 Conformado por un equipo altamente calificado, CODEPEC S.A.S. brinda acompañamiento en áreas clave como el derecho policivo, administrativo, urbanístico, laboral, comercial y de propiedad intelectual, garantizando seguridad jurídica y protección ante las decisiones de las autoridades.
 Nuestra experiencia nos permite ofrecer un servicio ágil, confiable y efectivo, convirtiéndonos en aliados estratégicos para establecimientos comerciales que buscan protección frente a sanciones, medidas correctivas y litigios, así como en procesos de negociación predial y estudios de títulos.
 </p>
 </div>                      
-
-<div className='seccionquienesyporque'>
 <h1 className='titulossecundariosinicio'>¿POR QUÉ ELEGIR CODEPEC S.A.S.?</h1>
+<div className='seccionquienesyporque'>
+
 <p className='textoquienesyporque'>
 <ul>    
 <li>Experiencia y especialización: Nuestro equipo cuenta con amplia trayectoria en la defensa de comerciantes y constructoras ante autoridades administrativas y de policía.</li>
@@ -41,10 +42,10 @@ Nuestra experiencia nos permite ofrecer un servicio ágil, confiable y efectivo,
 </ul>
 
 </p>
-</div>
+</div><h1 className='titulossecundariosinicio'>NUESTROS CLIENTES</h1>
 <div className='seccioncarrusel'>
-<h1 className='titulossecundariosinicio'>NUESTROS CLIENTES</h1>
-<Slider/>
+
+<Slider2/>
 </div>
 
 </div>
