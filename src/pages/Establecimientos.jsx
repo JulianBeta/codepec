200ca0f2d26c9f9a67e6ae309189151733721189
DiegoAlejandroRJ <diego6382@gmail.com>
2025-05-26 18:23:43 -0500

    terminados estilos textos paginas servicios

--- src/pages/Establecimientos.jsx
+++ src/pages/Establecimientos.jsx
@@ -6,94 +6,94 @@ import '../stylesheets/EstablecimientosyUrbanismo.css'
 export const Establecimientos = () => {
   return (
     <div className='contenedorpagina'>
-      <div className='titulo1'><p>SERVICIOS A ESTABLECIMIENTOS DE COMERCIO</p></div>
+      <div ><h1 className='titulo1'>SERVICIOS A ESTABLECIMIENTOS DE COMERCIO</h1></div>
 
-      <div className='titulo2'> <h2>ASESORÍA, ACOMPAÑAMIENTO Y REPRESENTACIÓN ANTE INSPECTORES DE POLICÍA POR MEDIDAS CORRECTIVAS</h2>
+      <div > <h2 className='titulo2'>ASESORÍA, ACOMPAÑAMIENTO Y REPRESENTACIÓN ANTE INSPECTORES DE POLICÍA POR MEDIDAS CORRECTIVAS</h2>
       </div>
-      <div className='titulo3'> 
-        <h3> Acciones específicas:
+      <div > 
+        <h3 className='titulo3'> Acciones específicas:
         </h3></div>
     
-      <div className='lista'> 
-        <ul>
-          <li>Elaboración, radicación y seguimiento de escritos de objeción a medidas correctivas.</li>
-          <li>Presentación de acciones de tutela por sellamientos ilegales.</li>
-          <li>	Representación legal y acompañamiento en audiencias ante inspectores de policía.</li></ul>
+      <div > 
+        <ul className='lista'>
+          <li>- Elaboración, radicación y seguimiento de escritos de objeción a medidas correctivas.</li>
+          <li>- Presentación de acciones de tutela por sellamientos ilegales.</li>
+          <li>-	Representación legal y acompañamiento en audiencias ante inspectores de policía.</li></ul>
       </div>
-      <div className='titulo3'><h3>Ventajas para el cliente:</h3></div>
-      <div className='lista'>
-        <ul>
-          <li>Protección ante sanciones arbitrarias o desproporcionadas.</li>
-          <li>Defensa legal efectiva para evitar el cierre del establecimiento.</li>
-          <li>Restablecimiento de la actividad comercial cuando ha sido afectada por decisiones administrativas.</li></ul>
+      <div ><h3 className='titulo3'>Ventajas para el cliente:</h3></div>
+      <div >
+        <ul className='lista'>
+          <li>- Protección ante sanciones arbitrarias o desproporcionadas.</li>
+          <li>- Defensa legal efectiva para evitar el cierre del establecimiento.</li>
+          <li>- Restablecimiento de la actividad comercial cuando ha sido afectada por decisiones administrativas.</li></ul>
       </div>
-      <div className='titulo2'> <h2>VERIFICACIÓN JURÍDICA DE CUMPLIMIENTO DE REQUISITOS PARA EL DESARROLLO DE ACTIVIDAD ECONÓMICA</h2>
+      <div > <h2 className='titulo2'>VERIFICACIÓN JURÍDICA DE CUMPLIMIENTO DE REQUISITOS PARA EL DESARROLLO DE ACTIVIDAD ECONÓMICA</h2>
       </div>
-      <div className = 'titulo3'><h3>
+      <div ><h3 className='titulo3'>
           Acciones específicas:</h3>
       </div>
-      <div> <ul>
-            <li>Revisión y validación de documentos exigidos por las autoridades.</li>
-            <li>		Asesoría y acompañamiento para garantizar el cumplimiento de todos los requisitos previstos el Artículo 87 del Código Nacional de Seguridad y Convivencia Ciudadana y demás normatividad aplicable.</li></ul>
+      <div> <ul className='lista'>
+            <li>- Revisión y validación de documentos exigidos por las autoridades.</li>
+            <li>- Asesoría y acompañamiento para garantizar el cumplimiento de todos los requisitos previstos el Artículo 87 del Código Nacional de Seguridad y Convivencia Ciudadana y demás normatividad aplicable.</li></ul>
       </div>
-       <div> <h3>Ventajas para el cliente:</h3></div>
-        <div><h4>
-          <ul>
-            <li>Prevención de sanciones y cierre de negocios.</li>
-            <li>	Cumplimiento legal garantizado para evitar conflictos con las autoridades.</li>
+       <div> <h3 className='titulo3'>Ventajas para el cliente:</h3></div>
+        <div>
+          <ul className='lista'>
+            <li>- Prevención de sanciones y cierre de negocios.</li>
+            <li>-	Cumplimiento legal garantizado para evitar conflictos con las autoridades.</li>
           </ul>
-        </h4></div>
+        </div>
 
-      <div> <h2>ASESORÍA EN CONTRATACIÓN LABORAL</h2></div>
-        <div><h4>
+      <div> <h2 className='titulo2'>ASESORÍA EN CONTRATACIÓN LABORAL</h2></div>
+        <div>
 
           Acciones específicas:
-        </h4></div>
-         <div> <ul>
+        </div>
+         <div> <ul className='lista'>
 
-            <li>	Elaboración y revisión de contratos laborales.</li>
-            <li>Cumplimiento normativo en términos de horas extras, turnos y prestaciones.</li>
-            <li>Asesoría en medidas disciplinarias y terminación de contratos.</li></ul></div>
+            <li>-	Elaboración y revisión de contratos laborales.</li>
+            <li>- Cumplimiento normativo en términos de horas extras, turnos y prestaciones.</li>
+            <li>- Asesoría en medidas disciplinarias y terminación de contratos.</li></ul></div>
 
-          <div><h3>Ventajas para el cliente:</h3></div>
-          <div><ul>
-            <li>  Reducción de riesgos legales y demandas laborales.</li>
-            <li>Optimización de la gestión de recursos humanos.</li>
+          <div><h3 className='titulo3'>Ventajas para el cliente:</h3></div>
+          <div><ul className='lista'>
+            <li>- Reducción de riesgos legales y demandas laborales.</li>
+            <li>- Optimización de la gestión de recursos humanos.</li>
 
           </ul></div>
 
-      <div> <h2>ESTRUCTURACIÓN DE CONTRATOS CON PROVEEDORES Y TERCEROS</h2></div>
-        <div><h3>
+      <div> <h2 className='titulo2'>ESTRUCTURACIÓN DE CONTRATOS CON PROVEEDORES Y TERCEROS</h2></div>
+        <div><h3 className='titulo3'>
           Acciones específicas:
         </h3></div>
-          <div><ul>
+          <div><ul className='lista'>
 
-            <li>Redacción y negociación de contratos comerciales.</li>
-            <li>Asesoría en contratos de arrendamiento, suministro y prestación de servicios.</li></ul></div>
-          <div><h3>Ventajas para el cliente:</h3>
+            <li>- Redacción y negociación de contratos comerciales.</li>
+            <li>- Asesoría en contratos de arrendamiento, suministro y prestación de servicios.</li></ul></div>
+          <div><h3 className='titulo3'>Ventajas para el cliente:</h3>
           <div>
-          <ul>
-            <li>Protección jurídica en acuerdos comerciales.</li>
-            <li>Minimización de riesgos de incumplimiento o fraudes.</li>
+          <ul className='lista'>
+            <li>- Protección jurídica en acuerdos comerciales.</li>
+            <li>- Minimización de riesgos de incumplimiento o fraudes.</li>
           </ul></div>
         </div>
 
 
-      <div> <h2>REPRESENTACIÓN EN PROCESOS POR COMPETENCIA DESLEAL Y PROPIEDAD INTELECTUAL</h2>
+      <div> <h2 className='titulo2'>REPRESENTACIÓN EN PROCESOS POR COMPETENCIA DESLEAL Y PROPIEDAD INTELECTUAL</h2>
       </div>
-       <div><h3>
+       <div><h3 className='titulo3'>
           Acciones específicas:
         </h3></div>
         <div>
-          <ul>
-            <li>Defensa ante prácticas de competencia desleal.</li>
-            <li>Protección y registro de derechos de propiedad intelectual.</li>
-            <li>Representación en acciones judiciales y extrajudiciales.</li></ul> </div>
-         <div> <h3>Ventajas para el cliente:</h3></div>
-          <div><ul><li>Protección ante sanciones arbitrarias o desproporcionadas.</li>
-            <li>Protección de la marca y activos intangibles.</li>
-            <li>Prevención de disputas comerciales.</li>
-            <li>Recuperación de daños por infracciones.</li>
+          <ul className='lista'>
+            <li>- Defensa ante prácticas de competencia desleal.</li>
+            <li>- Protección y registro de derechos de propiedad intelectual.</li>
+            <li>- Representación en acciones judiciales y extrajudiciales.</li></ul> </div>
+         <div> <h3 className='titulo3'>Ventajas para el cliente:</h3></div>
+          <div><ul className='lista'><li>Protección ante sanciones arbitrarias o desproporcionadas.</li>
+            <li>- Protección de la marca y activos intangibles.</li>
+            <li>- Prevención de disputas comerciales.</li>
+            <li>- Recuperación de daños por infracciones.</li>
 
           </ul></div>
           <GoToTop />
--- src/pages/Urbanismo.jsx
+++ src/pages/Urbanismo.jsx
@@ -6,43 +6,43 @@ import '../stylesheets/EstablecimientosyUrbanismo.css'
 export const Urbanismo = () => {
   return (
     <div className='contenedorpagina'>
-      <div className='titulo1' ><h1>SERVICIOS EN URBANISMO</h1></div>
+      <div  ><h1 className='titulo1'>SERVICIOS EN URBANISMO</h1></div>
 
-      <div className='titulo2'> <h2>ASESORÍA, ACOMPAÑAMIENTO Y REPRESENTACIÓN EN PROCESOS POLICIVOS POR INFRACCIONES URBANÍSTICAS</h2>
+      <div > <h2 className='titulo2'>ASESORÍA, ACOMPAÑAMIENTO Y REPRESENTACIÓN EN PROCESOS POLICIVOS POR INFRACCIONES URBANÍSTICAS</h2>
       </div>
-      <div className='titulo3'><h3>
+      <div ><h3 className='titulo3'>
         Acciones específicas:
       </h3></div>
-      <div className='lista'><ul>
-        <li>Defensa en procesos sancionatorios por infracciones urbanísticas.</li>
-        <li>Representación ante inspecciones de policía y autoridades urbanísticas.</li>
-        <li>Acompañamiento en la gestión de licencias y permisos.</li>
+      <div ><ul className='lista'>
+        <li>- Defensa en procesos sancionatorios por infracciones urbanísticas.</li>
+        <li>- Representación ante inspecciones de policía y autoridades urbanísticas.</li>
+        <li>- Acompañamiento en la gestión de licencias y permisos.</li>
       </ul>
-        <div className='titulo3'>  <h3>Ventajas para el cliente:</h3> </div>
+        <div >  <h3 className='titulo3'>Ventajas para el cliente:</h3> </div>
         <ul>
-          <li>Minimización del riesgo de sanciones y multas urbanísticas.</li>
-          <li>Regularización de proyectos en curso.</li>
-          <li>Defensa efectiva frente a procesos administrativos.</li>
+          <li>- Minimización del riesgo de sanciones y multas urbanísticas.</li>
+          <li>- Regularización de proyectos en curso.</li>
+          <li>- Defensa efectiva frente a procesos administrativos.</li>
         </ul>
       </div>
 
 
-      <div className='titulo2'> <h2>ELABORACIÓN DE ESTUDIOS DE TÍTULOS Y ACOMPAÑAMIENTO EN PROCESOS DE NEGOCIACIÓN PREDIAL</h2>
+      <div> <h2 className='titulo2'>ELABORACIÓN DE ESTUDIOS DE TÍTULOS Y ACOMPAÑAMIENTO EN PROCESOS DE NEGOCIACIÓN PREDIAL</h2>
       </div>
-      <div className='titulo3'><h3>
+      <div ><h3 className='titulo3'>
         Acciones específicas:
       </h3></div>
-      <div className='lista'> <ul>
-        <li>Análisis de la tradición jurídica de inmuebles.</li>
-        <li>Identificación de riesgos legales en la adquisición de propiedades.</li>
-        <li>Acompañamiento en procesos de negociación predial y conciliación.</li>
+      <div > <ul className='lista'>
+        <li>- Análisis de la tradición jurídica de inmuebles.</li>
+        <li>- Identificación de riesgos legales en la adquisición de propiedades.</li>
+        <li>- Acompañamiento en procesos de negociación predial y conciliación.</li>
       </ul>
       </div>
-      <div className='titulo3'><h3>Ventajas para el cliente:</h3></div>
-      <div className='lista'><ul>
-        <li>Seguridad en la adquisición de predios.</li>
-        <li>Reducción de riesgos legales en transacciones inmobiliarias.</li>
-        <li>Estrategias de negociación eficientes y seguras.</li>
+      <div><h3 className='titulo3'>Ventajas para el cliente:</h3></div>
+      <div ><ul className='lista'>
+        <li>- Seguridad en la adquisición de predios.</li>
+        <li>- Reducción de riesgos legales en transacciones inmobiliarias.</li>
+        <li>- Estrategias de negociación eficientes y seguras.</li>
       </ul>
       </div>
 
